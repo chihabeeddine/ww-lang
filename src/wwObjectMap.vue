cdf20149af5db964e1a6554c2937cf93feb1d824
fbriou <fbriou@gmail.com>
2019-05-06 11:07:18 +0200

    fix height issue and add styles

--- src/wwObjectMap.vue
+++ src/wwObjectMap.vue
@@ -85,9 +85,16 @@ export default {
 .ww-lang {
     position: relative;
     width: 150px;
+    height: 100%;
+    display: flex;
+    align-items: center;
     .lang-container {
+        position: absolute;
+        bottom: -5px;
+        transform: translateY(100%);
         display: flex;
         flex-direction: column;
+        align-items: center;
         width: 150px;
         background-color: white;
         border: 1px solid #979797;
@@ -95,8 +102,9 @@ export default {
         margin-top: 5px;
         visibility: hidden;
         opacity: 0;
-        transition: visibility 0s, opacity 0.8s linear;
+        transition: visibility 0s, opacity 0.5s linear;
         .lang {
+            width: 100%;
             padding: 5px 10px;
             cursor: pointer;
             &:hover {
@@ -114,6 +122,7 @@ export default {
         position: relative;
         .triangle {
             position: absolute;
+            right: -15px;
             bottom: 0;
             transform: translateY(-100%);
             margin-left: 5px;
@@ -121,7 +130,7 @@ export default {
             height: 0;
             border-style: solid;
             border-width: 8px 5px 0 5px;
-            border-color: #979797 transparent transparent transparent;
+            border-color: #2c2c2c transparent transparent transparent;
         }
     }
 }
